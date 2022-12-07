05a0956303087965aac0264e32ef43d06969c19b
Morgan <morganw2001@gmail.com>
2022-12-07 18:03:13 -0500

    added images for part h

--- Exam2/src/SyntaxAnalyzer.java
+++ Exam2/src/SyntaxAnalyzer.java
@@ -3,29 +3,8 @@ import java.util.ArrayList;
 
 public class SyntaxAnalyzer {
 
-    /*
-    * array of tokens
-    * identifier should be followed by an equals
-    * check list array of tokens to ensure syntax is correct
-    * PEMDAS is implemented with Stacks
-    *
-    * make sure it can output tokens, syntax is all about tokens
-    *
-    * what tokens are allowed to come before and after particular tokens
-    *
-    * */
 
 
-    public static void main (String [] args) {
-        LexicalAnalyzer.createTokensList();
-
-        ArrayList arrayList = LexicalAnalyzer.tokensList;
-
-        System.out.println(arrayList);
-
-
-
-    }
 
 
 
